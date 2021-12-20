28e927b80271f43e9434a4264501a714cc5047ab
KenNguyen <dinhvannguyen997@gmail.com>
2021-12-20 09:21:48 +0700

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,12 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"],
+    'react-native/no-unused-styles': 0,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 0,
+    'react-native/no-color-literals': 0,
+    'react-native/no-raw-text': 0,
+    'react-native/no-single-element-style-arrays': 0,
+    'no-console': 1,
   },
 };
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import {Text, View} from 'react-native';
+
+import React from 'react';
+
+let team = 0;
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
